7c4c9508f1e01eec544d1c277a89be9836444204
hocnv <hocnv@newwave.vn>
2023-12-05 15:53:13 +0700

    update filter

--- BEFALL23-DATN-SD05/src/main/java/com/example/befall23datnsd05/service/impl/ChiTietSanPhamCustomerServiceImpl.java
+++ BEFALL23-DATN-SD05/src/main/java/com/example/befall23datnsd05/service/impl/ChiTietSanPhamCustomerServiceImpl.java
@@ -2,16 +2,26 @@ package com.example.befall23datnsd05.service.impl;
 
 import com.example.befall23datnsd05.dto.AnhCustomerCustom;
 import com.example.befall23datnsd05.dto.ChiTietSanPhamCustomerCustom;
+import com.example.befall23datnsd05.dto.HDCT.ChiTietSanPhamDTO;
+import com.example.befall23datnsd05.dto.HDCT.ChiTietSanPhamMapper;
 import com.example.befall23datnsd05.entity.ChiTietSanPham;
+import com.example.befall23datnsd05.entity.SanPham;
 import com.example.befall23datnsd05.repository.ChiTietSanPhamCustomerRepository;
 import com.example.befall23datnsd05.service.ChiTietSanPhamCustomerService;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ChiTietSanPhamCustomerServiceImpl implements ChiTietSanPhamCustomerService {
@@ -73,6 +83,125 @@ public class ChiTietSanPhamCustomerServiceImpl implements ChiTietSanPhamCustomer
     @Override
     public ChiTietSanPham getById(Long id) {
         return repository.getReferenceById(id);
+    }
+
+    @Override
+    public ChiTietSanPhamDTO convertToDTO(ChiTietSanPham chiTietSanPham) {
+        return null;
+    }
+
+    @Override
+    public List<ChiTietSanPhamDTO> convertToDTOList(List<ChiTietSanPham> chiTietSanPhamList) {
+        return chiTietSanPhamList.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Page<ChiTietSanPhamDTO> findAllByCondition(
+            List<String> tenThuongHieu,
+            List<String> tenDongSanPham,
+            List<String> tenKichThuoc,
+            List<String> tenLotGiay,
+            List<String> tenCoGiay,
+            List<String> tenDeGiay,
+            List<String> tenMauSac,
+            Double minGia,
+            Double maxGia,
+            int page,
+            int pageSize,
+            String sortField
+    ) {
+        Specification<ChiTietSanPham> spec = (root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            Join<ChiTietSanPham, SanPham> sanPhamJoin = root.join("sanPham", JoinType.INNER);
+
+            if (tenThuongHieu != null && !tenThuongHieu.isEmpty()) {
+                predicates.add(sanPhamJoin.get("thuongHieu").get("ten").in(tenThuongHieu));
+            }
+            if (tenDongSanPham != null && !tenDongSanPham.isEmpty()) {
+                predicates.add(sanPhamJoin.get("dongSanPham").get("ten").in(tenDongSanPham));
+            }
+            if (tenDeGiay != null && !tenDeGiay.isEmpty()) {
+                predicates.add(root.get("deGiay").get("ten").in(tenDeGiay));
+            }
+            if (tenKichThuoc != null && !tenKichThuoc.isEmpty()) {
+                predicates.add(root.get("kichThuoc").get("ten").in(tenKichThuoc));
+            }
+            if (tenCoGiay != null && !tenCoGiay.isEmpty()) {
+                predicates.add(root.get("coGiay").get("ten").in(tenCoGiay));
+            }
+            if (tenLotGiay != null && !tenLotGiay.isEmpty()) {
+                predicates.add(root.get("lotGiay").get("ten").in(tenLotGiay));
+            }
+            if (tenMauSac != null && !tenMauSac.isEmpty()) {
+                predicates.add(root.get("mauSac").get("ten").in(tenMauSac));
+            }
+//            if (minPriceRanges != null && !minPriceRanges.isEmpty()) {
+//                List<Predicate> priceRangePredicates = new ArrayList<>();
+//
+//                for (String priceRange : minPriceRanges) {
+//                    double minPrice = 0.0;
+//                    double maxPrice = Double.MAX_VALUE;
+//
+//                    switch (priceRange) {
+//                        case "0-1":
+//                            maxPrice = 1000000.0;
+//                            break;
+//                        case "1-1.5":
+//                            minPrice = 1000000.0;
+//                            maxPrice = 1500000.0;
+//                            break;
+//                        case "1.5-2":
+//                            minPrice = 1500000.0;
+//                            maxPrice = 2000000.0;
+//                            break;
+//                        case "2-2.5":
+//                            minPrice = 2000000.0;
+//                            maxPrice = 2500000.0;
+//                            break;
+//                        case "2.5-3":
+//                            minPrice = 2500000.0;
+//                            maxPrice = 3000000.0;
+//                            break;
+//                        case "3+":
+//                            minPrice = 3000000.0;
+//                            break;
+//                        default:
+//                            // Xử lý các khoảng giá khác nếu cần
+//                            break;
+//                    }
+//                    // Tạo một Predicate cho mỗi khoảng giá
+//        Predicate pricePredicate = (Predicate) cb.between(root.get("gia"), minPrice, maxPrice);
+//        priceRangePredicates.add(pricePredicate);
+//    }
+//
+//    // Tạo một Predicate gồm logic OR cho tất cả các khoảng giá
+//    Predicate priceRangeOrPredicate = cb.or(priceRangePredicates.toArray(new Predicate[0]));
+//
+//    // Thêm Predicate logic OR vào danh sách các Predicate
+//                predicates.add(priceRangeOrPredicate);
+//}
+            return cb.and(predicates.toArray(new Predicate[0]));
+        };
+
+        // Sử dụng ChiTietGiayRepository và PageRequest để lấy kết quả phân trang
+        Sort sort = Sort.unsorted();
+
+        if ("0".equals(sortField)) {
+            sort = Sort.by("giaBan").ascending();
+        } else if ("1".equals(sortField)) {
+            sort = Sort.by("giaBan").descending();
+        }
+        else if ("2".equals(sortField)) {
+            sort = Sort.by("sanPham.ten").ascending();
+        } else if ("3".equals(sortField)) {
+            sort = Sort.by("ngayTao").descending();
+        }
+
+        Pageable pageable = PageRequest.of(page, pageSize, sort);
+
+        return ChiTietSanPhamMapper.toDTOPage(repository.findAll(spec, pageable));
     }
 
 
